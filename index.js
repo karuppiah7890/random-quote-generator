a3f31b21d28aaf660734f70ab14376d07f71ed8b
Karuppiah <karuppiah7890@gmail.com>
2016-05-09 01:17:29 +0530

    Change Tweet URL based on quote

--- index.js
+++ index.js
@@ -21,4 +21,5 @@ function gotQuote(data){
     console.log(data);
     $("#quote").html(data.quoteText);
     $("#author").html(data.quoteAuthor);
+    $("#tweet-link").attr("href","https://twitter.com/intent/tweet?text=" + data.quoteText + "by%20\"" + data.quoteAuthor + "\"&related=freecode%20camp&hashtags=quotes");
 }
