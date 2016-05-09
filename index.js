7c29a57cc6494c357b0addf04ffae0f453da26d6
Karuppiah <karuppiah7890@gmail.com>
2016-05-09 13:02:33 +0530

    Add button for refresh and add css

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+    getTheQuote();
+
+    $("#newQuote").on("click",function(){
+      getTheQuote();
+    });
+});
+
+
+function getTheQuote() {
   $.ajax({
       url : "http://api.forismatic.com/api/1.0/",
       type : "POST",
@@ -13,13 +22,22 @@ $(document).ready(function(){
       jsonp : "jsonp",
       jsonpCallback : "gotQuote"
   });
-
-});
+}
 
 function gotQuote(data){
 
+    var quoteAuthor;
+
     console.log(data);
     $("#quote").html(data.quoteText);
-    $("#author").html(data.quoteAuthor);
-    $("#tweet-link").attr("href","https://twitter.com/intent/tweet?text=" + data.quoteText + "by%20\"" + data.quoteAuthor + "\"&related=freecode%20camp&hashtags=quotes");
+
+    if(data.quoteAuthor!=="")
+    quoteAuthor = data.quoteAuthor;
+
+    else
+    quoteAuthor = "Unknown Source";
+
+    $("#author").html(quoteAuthor);
+
+    $("#tweet-link").attr("href","https://twitter.com/intent/tweet?text=" + data.quoteText + "by%20\"" + quoteAuthor + "\"&related=freecode%20camp&hashtags=quotes");
 }
